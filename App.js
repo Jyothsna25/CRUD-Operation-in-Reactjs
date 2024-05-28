06656dd2a5e5b6601796b483d2dbeb362116a1d5
Jyothsna <jyothsnajyo34@gmail.com>
2024-05-28 13:37:00 +0530

    This is Crud operations

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CrudOperations from './crud';
+
+function App() {
+  return (
+    <div >
+      <CrudOperations/>
+    </div>
+  );
+}
+
+export default App;
